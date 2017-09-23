d25a6d842cbcaaf639b1f5d980af92779a755c48
zakariyah <zakariyahsoroye@gmail.com>
2017-09-23 23:00:55 +0400

    ip changed

--- public/javascripts/ActionGUI.js
+++ public/javascripts/ActionGUI.js
@@ -436,8 +436,8 @@ var PrisonersDilemma = function()
 	var gametypeinspectorid = document.getElementById("gametypeinspectorid").innerHTML;
 
 		
-	var socket = io.connect('http://localhost:4000');
-	// var socket = io.connect('http://ec2-52-88-237-252.us-west-2.compute.amazonaws.com:4000/');
+	// var socket = io.connect('http://localhost:4000');
+	var socket = io.connect('http://ec2-52-88-237-252.us-west-2.compute.amazonaws.com:4000/');
 	var myCanvasContainer =  new CanvasContainer(socket);
 
 	var blocker = new Blocker();
