441fd878687c9225de4b36be14625479e0e4ff4e
zakariyah <zakariyahsoroye@gmail.com>
2017-10-05 19:56:54 +0400

    double game fixed

--- controller/gameControllerArray.js
+++ controller/gameControllerArray.js
@@ -12,7 +12,7 @@ var gameControllerArray = function()
 		var start_game = start_game_results.start_game;
 		var players = start_game_results.players;
 		var gameType = start_game_results.gameType;
-		console.log("results " + start_game_results);
+		console.log("results " + JSON.stringify(start_game_results));
 		console.log("socket id  " + socket_id);
 		console.log("hiitNumber " + hiitNumber);		
 
@@ -33,16 +33,19 @@ var gameControllerArray = function()
 			{
 				var controller_game = null;
 				console.log("game_controllers_hiit " + Object.keys(game_controllers_hiit));
-				for(hiit_no in players)
+				for(hiit_no_ind in players)
 				{
+					var hiit_no = players[hiit_no_ind];
 					if(hiit_no in game_controllers_hiit)
 					{
+						console.log("game_controllers_in " + hiit_no + " id " + players);
 						controller_game = game_controllers_hiit[hiit_no];
 						game_controllers_hiit[hiitNumber] = controller_game;
 						game_controllers[socket_id] = controller_game;
 
 						return controller_game;
 					}
+					console.log("game_controllers_out " + hiit_no + " id " + players);
 				}
 			}
 		}
